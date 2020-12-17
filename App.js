75dab7864f10d5e473d54deefffb68ac31df9322
suhaan03 <suhaanshah03@gmail.com>
2020-12-17 16:40:04 +0530

    drawer added

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
+
+import LoginScreen from './screens/LoginScreen';
+import { AppDrawerNavigator } from './components/AppDrawerNavigator'
+import { AppTabNavigator} from './components/AppTabNavigator'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContainer/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const switchNavigator = createSwitchNavigator({
+  LoginScreen:{screen: LoginScreen},
+  Drawer:{screen: AppDrawerNavigator}
+})
+
+const AppContainer =  createAppContainer(switchNavigator);
